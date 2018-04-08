74fa9abd3c46bdf8bda3ce34373a8499ceff8274
Box-Of-Hats <ja1ke@hotmail.co.uk>
2018-04-08 16:15:07 +0100

    Created basic utility function calculations for a given board

--- CheatAnalyser.py
+++ CheatAnalyser.py
@@ -1,3 +1,29 @@
+from ChessPiece import *
+
+
 class CheatAnalyser():
     def __init__(self):
-        pass
+        self.piece_values = {
+            Pawn: 1,
+            King: 10000,
+            Queen: 20,
+            Rook: 8,
+            Knight: 10,
+            Bishop: 8,
+        }
+
+    def get_score_for_board(self, board, player_id):
+        """
+        Get the score of a given board state.
+        Simply sums the scores of all pieces on the board.
+        """
+        score = 0
+        for row in board:
+            for piece in row:
+                if isinstance(piece, ChessPiece):
+                    if piece.owner_id == player_id:
+                        score += self.piece_values[type(piece)]
+                    else:
+                        score -= self.piece_values[type(piece)]
+
+        return score
